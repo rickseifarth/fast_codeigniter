cb0226391b72615f91adb46350bc02cb17a5a653
unknown <rickseifarth@hotmail.com>
2024-11-23 22:10:02 -0300

    Alteradas as mensagens de alertas para que o usuário possa fecha-las

--- app/Views/layout/messages.php
+++ app/Views/layout/messages.php
@@ -1,7 +1,7 @@
 <?php if (session('error') !== null) : ?>
-    <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+    <div class="alert alert-danger alert-dismissible" role="alert"><?= session('error') ?><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>
 <?php elseif (session('errors') !== null) : ?>
-    <div class="alert alert-danger" role="alert">
+    <div class="alert alert-danger alert-dismissible" role="alert">
         <?php if (is_array(session('errors'))) : ?>
             <?php foreach (session('errors') as $error) : ?>
                 <?= $error ?>
@@ -10,9 +10,10 @@
         <?php else : ?>
             <?= session('errors') ?>
         <?php endif ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 <?php endif ?>
 
 <?php if (session('message') !== null) : ?>
-    <div class="alert alert-success" role="alert"><?= session('message') ?></div>
+    <div class="alert alert-success alert-dismissible" role="alert"><?= session('message') ?><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>
 <?php endif ?>
